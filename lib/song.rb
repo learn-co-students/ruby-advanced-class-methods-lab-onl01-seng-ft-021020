ba56e362afa7fc6199426f7cb1dcc475c079779a
Urika Pye <urikapyearies@gmail.com>
2020-01-11 18:45:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
+  @@all = [song = Song.create
+  Song.all.include?(song)]
 
   def self.all
     @@all
@@ -11,3 +12,26 @@ class Song
   end
 
 end
+
+s = Song.new
+s.name = "The Ship Song"
+s.artist_name = "Nick Cave"
+s.save
+
+
+song = Song.new_by_name("The Middle")
+song.name
+
+song = Song.creat_by_name("The Middle")
+song
+Song.all.include?(song)
+
+the_middle = Song.create_by_name("The Middle")
+Song.find_by_name("The Middle")
+
+song_1 = Song.find_or_create_by_name("Blank Space")
+song_2 = Song.find_or_create_by_name("Blank Space")
+song_1 == song_2
+
+song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
+song.namesong.artist_name
